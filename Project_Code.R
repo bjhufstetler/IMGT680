c61032df9a2d1610cdaf091b142bf4552db2d132
bjhufstetler <bjhufstetler@gmail.com>
2019-07-24 11:10:31 -0400

    Project Code

---
+++ Project_Code.R
@@ -0,0 +1,8 @@
+setwd("~/IMGT680")
+load("crimedata1.RData")
+load("crimedata2.RData")
+load("crimedata3.RData")
+load("crimedata4.RData")
+load("crimedata5.RData")
+
+data <- rbind(crime.data.1,crime.data.2,crime.data.3,crime.data.4,crime.data.5)
