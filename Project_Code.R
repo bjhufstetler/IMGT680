b032b59ee9fce7d7227f342ea226bbde1d6e9b0a
bjhufstetler <bjhufstetler@gmail.com>
2019-07-25 07:37:34 -0400

    Changed data import to single data file.

--- Project_Code.R
+++ Project_Code.R
@@ -2,14 +2,7 @@
   #register_google(key = "AIzaSyAxPCoK5JzfwK5A-7tvfYrk2L7Aiz99tZA", write=TRUE)
 
 setwd("~/IMGT680")
-load("crimedata1.RData")
-load("crimedata2.RData")
-load("crimedata3.RData")
-load("crimedata4.RData")
-load("crimedata5.RData")
-
-data <- rbind(crime.data.1,crime.data.2,crime.data.3,crime.data.4,crime.data.5)
-
+load("BostonCrimeData2015-2018.RData")
 
 require(stringr)
 regex <-"\\((\\d+\\.\\d+), (-\\d+\\.\\d+)\\)"
